1c5f7ec525c0d7f6ed820dc11e8e42de41102088
David Guilherme <47979584+DGuilherme@users.noreply.github.com>
2022-11-21 23:47:39 +0000

    final commit

--- game.js
+++ game.js
@@ -40,21 +40,23 @@ window.onload = () => {
 
     foodPlace();
 
-    setInterval(update, 1000 / 10);
+    setInterval(update, 1000 / 10)
 }
 
-function update (){
+function update() {
 
-    // Clear Screen
-    createRectangle(0, 0, board.width, board.height);
+    // Clear screen
+    createRect(0, 0, board.width, board.height)
 
-    if (gameOver){
+    if (gameOver) {
 
-        createText('Game Over', board.width / 2, board.height / 2 - 25, 'center', 50 );
+        // Game end screen
+
+        createText(`Game Over`, board.width / 2, board.height / 2 - 25, 'center', 50);
 
         createText(`Score: ${score}`, board.width / 2, board.height / 2 + 25, 'center');
 
-        createText('Click to Start Again', (cols * blockSize) / 2, board.height - 50, 'center');
+        createText(`Click to Start Again`, (cols * blockSize) / 2, board.height - 50, 'center');
 
         return
     }
@@ -63,25 +65,25 @@ function update (){
     createText(`Score: ${score}`, 30, 40);
 
     // Create first food
-    createRectangle(foodX, foodY, blockSize, blockSize, 'lime');
+    createRect(foodX, foodY, blockSize, blockSize, "lime");
 
-    // Eat action
-    if (snakeX == foodX && snakeY == foodY){
-        tail.push([foodX, foodY])
+    // Did it eat
+    if (snakeX == foodX && snakeY == foodY) {
+        tail.push([foodX, foodY]);
 
         score += 10;
 
         appleAudio.play();
 
-        foodPlace();
+        foodPlace()
     }
 
-    // Snake Tail
-    for (let i = tail.length -1; i > 0; i-- ){
+    // Snake tail
+    for (let i = tail.length - 1; i > 0; i--) {
         tail[i] = tail[i - 1];
     }
 
-    if (tail.length){
+    if (tail.length) {
         tail[0] = [snakeX, snakeY];
     }
 
@@ -89,73 +91,64 @@ function update (){
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
 
-    createRectangle(snakeX, snakeY, blockSize, blockSize,'orange');
+    createRect(snakeX, snakeY, blockSize, blockSize, 'orange');
 
-    for (let i = 0; i < tail.length; i++){
-        createRectangle(tail[i][0], tail[i][1], blockSize, blockSize, 'lime');
+    for (let i = 0; i < tail.length; i++) { 
+        createRect(tail[i][0], tail[i][1], blockSize, blockSize, 'lime');
     }
 
     // Hit the wall
-    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize){
-        gameOverEvent();
+    if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+        gameOverEvent()
     }
 
     // Shot herself
-    for (let i = 0; i < tail.length; i++){
-        if(snakeX == tail[i][0] && snakeY == tail[i][1]){
-            gameOverEvent();
+    for (let i = 0; i < tail.length; i++) {
+        if (snakeX == tail[i][0] && snakeY == tail[i][1]) {
+            gameOverEvent()
         }
     }
-
 }
 
-function foodPlace(){
+function foodPlace() {
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * rows) * blockSize;
 }
 
-function changeDirection(e){
-
-    if (e.code == "ArrayUp"){
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
         velocityX = 0;
         velocityY = -1;
-    }else if (e.code == "ArrayDown"){
+    } else if (e.code == "ArrowDown") {
         velocityX = 0;
         velocityY = 1;
-    }else if (e.code == "ArrayLeft"){
+    } else if (e.code == "ArrowLeft") {
         velocityX = -1;
         velocityY = 0;
-    }else if (e.code == "ArrayRight"){
+    } else if (e.code == "ArrowRight") {
         velocityX = 1;
         velocityY = 0;
     }
 }
 
-function gameOverEvent(){
+function gameOverEvent() {
     gameOver = true;
-
     gameOverAudio.play();
-
     tail = [];
-
     snakeX = 0;
     snakeY = 0;
-
     velocityX = 1;
     velocityY = 0;
-
 }
 
-function createRectangle(x, y, width, height, color = "black"){
+function createRect(x, y, width, height, color = "black") {
     context.fillStyle = color;
     context.fillRect(x, y, width, height);
 }
 
-function createText(text, x, y, textAlign = "start", fontSize = 20){
-    context.fillStyle = 'lime';
+function createText(text, x, y, textAlign = "start", fontSize = 20 ) {
+    context.fillStyle = "lime";
     context.font = `${fontSize}px Roboto Mono`;
     context.textAlign = textAlign;
-    context.fillText(text, x, y) 
+    context.fillText(text, x, y)
 }
-
-
